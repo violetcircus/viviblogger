041b62693a0122a6465c32d7ddd1e239bcdfad8f
violet <iaresmarts@gmail.com>
2025-07-26 15:55:44 +0100

    lol blog post i read to get started had ridiculous code that i copied without thinking too hard about it. this is much better

--- markdown/heading.go
+++ markdown/heading.go
@@ -10,24 +10,8 @@ func handleHeadings(line []byte, content string, builder *strings.Builder) {
 	if line[0] == '#' {
 		// count number of #s at beginning of line
 		count := bytes.Count(line, []byte("#"))
-		switch count {
-		case 1:
-			headingReplacer(content, 1, builder)
-		case 2:
-			headingReplacer(content, 2, builder)
-		case 3:
-			headingReplacer(content, 3, builder)
-		case 4:
-			headingReplacer(content, 4, builder)
-		case 5:
-			headingReplacer(content, 5, builder)
-		case 6:
-			headingReplacer(content, 6, builder)
-		}
-	}
-}
+		str := strings.Replace(content, "#", "", -1)
 
-func headingReplacer(line string, num int, builder *strings.Builder) {
-	str := strings.Replace(line, "#", "", -1)
-	builder.WriteString(fmt.Sprintf("<h%d>%s</h%d>", num, strings.TrimSpace(str), num))
+		builder.WriteString(fmt.Sprintf("<h%d>%s</h%d>", count, strings.TrimSpace(str), count))
+	}
 }
